07f9718bd2f8736198fdd91b8d1c7d5462f4f49a
Israel Peixoto <israel1peixoto@gmail.com>
2024-06-11 13:31:08 -0300

    first commit

---
+++ src/main/java/com/example/israelpx/IsraelpxApplication.java
@@ -0,0 +1,13 @@
+package com.example.israelpx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsraelpxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsraelpxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/israelpx/IsraelpxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.israelpx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsraelpxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
